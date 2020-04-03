317f909026c572fe894d09d2bc6a806885d3bb5e
Jack Cooper <jackcooper1245@gmail.com>
2020-04-03 10:10:38 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0 
-    return "X"
-  else 
-    return "O"
+  if turn_count(board) % 2 == 0 ? "X" : "O"
+   
  end
 end
